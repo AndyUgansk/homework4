673029f755ce2ad0f70233ba060e87c67ed6f5ac
AndyUgansk <andyugansk@gmail.com>
2024-07-30 09:07:58 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = "строка с текстом"
+n = len(my_string)
+print(n)
+print(my_string.upper()) # строка в верхнем регистре
+print(my_string.lower()) # строка в нижнем регистре
+print(my_string.replace(" ", "")) # строка без пробелов
+print(my_string[0]) # первый символ строки
+print(my_string[-1]) # последний символ строки
